734eb4eabf27bd86dd0b4a041c3fbf26c857129a
Jaco <jaco@dittojobs.com>
2021-09-05 08:47:29 +0200

    invoice detail page with dynamic data

--- src/pages/InvoiceDetail.tsx
+++ src/pages/InvoiceDetail.tsx
@@ -36,45 +36,13 @@ const InvoiceDetail = (props) => {
     total,
   } = invoiceSelected;
 
-  // const fff = {
-  //   id: "RG0314",
-  //   createdAt: "2021-09-24",
-  //   paymentDue: "2021-10-01",
-  //   description: "Website Redesign",
-  //   paymentTerms: 7,
-  //   clientName: "John Morrison",
-  //   clientEmail: "jm@myco.com",
-  //   status: "paid",
-  //   senderAddress: {
-  //     street: "19 Union Terrace",
-  //     city: "London",
-  //     postCode: "E1 3EZ",
-  //     country: "United Kingdom",
-  //   },
-  //   clientAddress: {
-  //     street: "79 Dover Road",
-  //     city: "Westhall",
-  //     postCode: "IP19 3PF",
-  //     country: "United Kingdom",
-  //   },
-  //   items: [
-  //     {
-  //       name: "Website Redesign",
-  //       quantity: 1,
-  //       price: 14002.33,
-  //       total: 14002.33,
-  //     },
-  //   ],
-  //   total: 14002.33,
-  // };
-
   return (
     <>
       <div style={{ textAlign: "left", padding: "20px" }}>
-        <a href="/" style={{ textAlign: "left", textDecoration: "none" }}>
+        <Link to="/" style={{ textAlign: "left", textDecoration: "none" }}>
           {" "}
           {`<  Go Back`}{" "}
-        </a>
+        </Link>
       </div>
       <div
         style={{
